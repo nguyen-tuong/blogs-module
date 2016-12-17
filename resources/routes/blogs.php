20117602e1f038523a11c52941e043fa38663a66
buinguyentuong <mrbuinguyentuong94@gmail.com>
2016-12-18 02:00:16 +0700

    update route

--- resources/routes/blogs.php
+++ resources/routes/blogs.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'admin/blogs/blogs' => 'Anomaly\BlogsModule\Http\Controller\Admin\BlogsController@index',
-    'admin/blogs/blogs/create' => 'Anomaly\BlogsModule\Http\Controller\Admin\BlogsController@create',
-    'admin/blogs/blogs/edit/{id}' => 'Anomaly\BlogsModule\Http\Controller\Admin\BlogsController@edit'
+    'admin/blogs' => 'Anomaly\BlogsModule\Http\Controller\Admin\BlogsController@index',
+    'admin/blogs/create' => 'Anomaly\BlogsModule\Http\Controller\Admin\BlogsController@create',
+    'admin/blogs/edit/{id}' => 'Anomaly\BlogsModule\Http\Controller\Admin\BlogsController@edit'
 ];
